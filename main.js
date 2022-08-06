49b235cb0b33c96436dbe6e3b42dd7668feb8886
ZEugenii <zinich911@gmail.com>
2022-08-06 15:04:43 +0300

    solve tusk 1

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello world")
+//Напишіть програму, яка отримає від користувача
+//число (кількість хвилин) і виведе у консоль
+//Рядок у форматі годин і хвилин
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+// 70 === 01:10
+
+const time = Number(prompt("Ведіть кількість хвилин"));
+// console.log( typeof time);
+
+let hours = time / 60;
+hours = Math.floor(hours);
+hours = String(hours).padStart(2, 0);
+
+// let minutes = time - hours * 60;
+let minutes = time % 60;
+minutes = String(minutes).padStart(2, 0);
+
+console.log(`${hours}:${minutes}`);
