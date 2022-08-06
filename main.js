d04f0c24e8ecfc91a0773e0c979ce839b09c8fda
Misha Panivnyk <panivnykm@gmail.com>
2022-08-06 16:30:01 +0300

    task-5

--- main.js
+++ main.js
@@ -17,8 +17,6 @@
 
 // console.log(`${hours}:${minutes}`);
 
-
-
 // Task-2_____________________________________
 
 //Використовуючи функцію if...else,
@@ -28,12 +26,10 @@
 // показати через alert: "Вірно!"
 //інакше відобразити:"Не знаєте? ECMAScript!"
 
-
 // const userInputInLowerCase = prompt("Яка офіційна назва JavaScript?").toLowerCase();
 
 // console.log(userInputInLowerCase);
 // const officialName = "ECMAScript".toLowerCase();
-
 
 // // if (userInputInLowerCase === officialName) {
 // //   alert("Вірно!");
@@ -52,17 +48,14 @@
 // const min = 23;
 // let sum = 0;
 // for (let i = max; i > min; i-=1) {
-    
+
 //     if (i % 2 !== 0) {
-      
+
 //         continue;
 //     }
 //     sum += i;
 // }
 // console.log(sum);
-
-
-
 
 //При завантаженні сторінки користувачеві пропонується
 //В prompt ввести число. Введене значення додається до значення
@@ -74,13 +67,25 @@
 //Робити перевірку,що користувач ввів саме число,
 //а не довільний набір символів не потрібно.
 
+// let number = Number(prompt("Введіть число"));
+// let total = 0;
 
-let number = Number(prompt("Введіть число"));
-let total = 0;
+// while (number) {
+//     total += number;
+//     number = Number(prompt("Введіть число"));
 
-while (number) {
-    total += number;
-    number = Number(prompt("Введіть число"));
-    
+// }
+//     alert(total);
+//Напишіть цикл, який пропонує ввести
+//число більше 100 через prompt.
+//Якщо відвідувач ввів інше число - попросити
+//Ввести ще раз і так далі.
+//Цикл має запитувати число, доки відвідувач не
+//введе число більше 100, або натисне кнопку
+//Скасування в prompt
+//Потрібно додати перевірку, що користувач вводить числа
+let number = prompt("Введіть число більше 100");
+while (number < 100 && number !== null) {
+  number = prompt("Введіть число більше 100");
 }
-    alert(total);
+console.log(`Ви ввели ${number}`);
