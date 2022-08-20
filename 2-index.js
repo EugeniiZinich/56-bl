d40f42934424d343ff31f346095fdac25eddbb28
AndriiLendiel <lendiel9333@gmail.com>
2022-08-20 15:22:56 +0300

    task-2-3
    t23

--- 2-index.js
+++ 2-index.js
@@ -43,19 +43,51 @@
 ? Повинно вийти 390. Якщо об'єкт salaries порожній, результат має бути 0.
 */
 
-const salaries = {
-  John: 100,
-  Ann: 160,
-  Pete: 130,
-};
+// const salaries = {
+//   John: 100,
+//   Ann: 160,
+//   Pete: 130,
+// };
 
-const totalSalaries = arr => {
-  const values = Object.values(arr);
-  let sum = 0;
-  for (const value of values) {
-    sum += value;
+// const totalSalaries = arr => {
+//   const values = Object.values(arr);
+//   let sum = 0;
+//   for (const value of values) {
+//     sum += value;
     
-  }
-  return sum;
-}
-console.log(totalSalaries(salaries));
+//   }
+//   return sum;
+// }
+// console.log(totalSalaries(salaries));
+
+
+/*
+? Напишіть ф-цію calcTotalPrice(stones, stoneName), яка приймає масив об'єктів та рядок
+ під назвою каменю.
+? Ф-ція рахує і повертає загальну вартість каміння з таким ім'ям.
+*/
+
+// const stones = [
+//   { name: 'Смарагд', price: 1300, quantity: 4 },
+//   { name: 'Діамант', price: 2700, quantity: 3 },
+//   { name: 'Сапфір', price: 400, quantity: 7 },
+//   { name: 'Щебінь', price: 200, quantity: 2 },
+//   { name: 'Смарагд', price: 1300, quantity: 4 }
+// ];
+
+
+// const calcTotalPrice = (stones, stoneName) => {
+//   let totalPrice = 0;
+//   for (const stone of stones) {
+//     if (stone.name === stoneName) {
+//       console.log(stone);
+      
+//       totalPrice += (stone.price * stone.quantity);
+
+//     }
+    
+//   }
+//     return totalPrice
+// }
+
+// console.log(calcTotalPrice(stones, 'Смарагд' ));
