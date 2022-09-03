0638ff5296fa8e8fe31c788790eaecfa1e533d1c
ViacheslavMiakota <parazyt2010@gmail.com>
2022-09-03 14:56:16 +0300

    add tusk3-1

--- 3-index.js
+++ 3-index.js
@@ -3,13 +3,29 @@
 // Назначить скидку 20% на фрукты в массиве,
 // //присвоить id для каждого продукта
 
-const fruits = [
-  { name: "apple", price: 200 },
-  { name: "orange", price: 300 },
-  { name: "grapes", price: 750 },
-];
+// const fruits = [
+//   { name: "apple", price: 200 },
+//   { name: "orange", price: 300 },
+//   { name: "grapes", price: 750 },
+// ];
 
-const changedFruits = fruits.map((fruit) => {
-  return { ...fruit, price: fruit.price * 0.8, id: Date.now() };
-});
-console.log(changedFruits);
+// const changedFruits = fruits.map((fruit) => {
+//   return {
+//     ...fruit,
+//     price: fruit.price * 0.8,
+//     id: Date.now(),
+//   };
+// });
+// console.log(changedFruits);
+
+const items = [
+  { name: "SAMSUNG", price: 15000 },
+  { name: "LG", price: 9000 },
+  { name: "DELL", price: 27000 },
+  { name: "BENQ", price: 12000 },
+  { name: "ASER", price: 7000 },
+];
+const sortedItems = [...items].sort(
+  (a, b) => a.price - b.price
+);
+console.log(sortedItems);
